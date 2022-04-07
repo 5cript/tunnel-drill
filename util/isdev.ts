9d277d04e2e876224282b75b2d80a783ec9af501
Tim Ebbeke <tim06tr@gmail.com>
2022-04-07 16:08:57 +0200

    isDev fixed.

--- util/isdev.ts
+++ util/isdev.ts
@@ -1,4 +1,6 @@
 const isDev = () => {
+    if (process.env.DEV_BUILD === undefined || process.env.DEV_BUILD === null)
+        return false;
     return process.env.DEV_BUILD === "1" || process.env.DEV_BUILD.toLowerCase() === "true"
 }
 
