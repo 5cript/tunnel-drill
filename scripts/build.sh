e2473669da238fdb18b25d4f0c2d9cbc8c56c07c
Tim Ebbeke <tim06tr@gmail.com>
2022-04-10 16:13:33 +0200

    Made scripts cross platform.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-ISCLANG=on
+IS_MSYS2_CLANG=off
 COMPILER=clang++
 CCOMPILER=clang
 LINKER=lld
 THREADS=1
+BUILD_TYPE=Debug
 
 # Go to build dir
 CURDIR=$(basename $PWD)
@@ -12,19 +13,32 @@ if [[ $CURDIR == "scripts" ]]; then
   cd ..
 fi
 mkdir -p build
-mkdir -p build/clang_debug
-cd build/clang_debug
+mkdir -p "build/${CCOMPILER}_${BUILD_TYPE,,}"
+cd "build/${CCOMPILER}_${BUILD_TYPE,,}"
 
 export CXX=$COMPILER
 export CC=$CCOMPILER
 
+CMAKE_GENERATOR="Unix Makefiles"
+if [[ ! -z "${MSYSTEM}" ]]; then
+  CMAKE_GENERATOR="MSYS Makefiles"
+  if [[ $COMPILER == clang ]]; then
+    IS_MSYS2_CLANG=on
+  fi
+fi
+
+CMAKE_CXX_FLAGS=""
+if [[ $COMPILER == clang ]]; then
+  CMAKE_CXX_FLAGS="-fuse-ld=lld"
+fi
+
 cmake \
-  -G"MSYS Makefiles" \
-  -DMSYS2_CLANG=$ISCLANG \
-  -DCMAKE_BUILD_TYPE=Debug \
+  -G"${CMAKE_GENERATOR}" \
+  -DMSYS2_CLANG=$IS_MSYS2_CLANG \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DJWT_DISABLE_PICOJSON=on \
   -DJWT_BUILD_EXAMPLES=off \
-  -DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
+  -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS \
   -DCMAKE_CXX_COMPILER=$COMPILER \
   -DCMAKE_C_COMPILER=$CCOMPILER \
   -DCMAKE_LINKER=$LINKER \
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -e
 
+CCOMPILER=clang
+BUILD_TYPE=Debug
+
 CURDIR=$(basename $PWD)
 if [[ $CURDIR == "scripts" ]]; then
   cd ..
 fi
 
+EXE=broker
+if [[ ! -z "${MSYSTEM}" ]]; then
+  EXE=broker.exe
+fi
+
 bash ./scripts/build.sh && \
-    ./build/clang_debug/bin/broker.exe
+    "./build/${CCOMPILER}_${BUILD_TYPE,,}/bin/${EXE}"
