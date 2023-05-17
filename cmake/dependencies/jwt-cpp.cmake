95d28fa9e55b4f89179f8d5825dab55c8bee0c74
Tim Ebbeke <tim06tr@gmail.com>
2023-05-17 02:54:29 +0200

    Bumped jwt library.

--- cmake/dependencies/jwt-cpp.cmake
+++ cmake/dependencies/jwt-cpp.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	jwt-cpp
 	GIT_REPOSITORY https://github.com/Thalhammer/jwt-cpp.git
-	GIT_TAG        03ad09b996fb85f8fc6645795f89c39940bc4902    
+	GIT_TAG        4e3dd40cfef9f06fa48c8a8d804405ec196885cf    
 )
 FetchContent_MakeAvailable(jwt-cpp)
