145eb1916619f6272698530b2b1ebe8ee6e67f70
Tim Ebbeke <tim06tr@gmail.com>
2023-08-18 21:00:31 +0200

    Bumped jwt-cpp

--- cmake/dependencies/jwt-cpp.cmake
+++ cmake/dependencies/jwt-cpp.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	jwt-cpp
 	GIT_REPOSITORY https://github.com/Thalhammer/jwt-cpp.git
-	GIT_TAG        4e3dd40cfef9f06fa48c8a8d804405ec196885cf    
+	GIT_TAG        ce1f9df3a9f861d136d6f0c93a6f811c364d1d3d    
 )
 FetchContent_MakeAvailable(jwt-cpp)
