62743f2c3453cf4b9a94a6c113a2d13b3a18f7c4
Tim Ebbeke <tim06tr@gmail.com>
2024-09-19 00:56:36 +0200

    Updated roar.

--- cmake/dependencies/roar.cmake
+++ cmake/dependencies/roar.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	roar
 	GIT_REPOSITORY https://github.com/5cript/roar.git
-	GIT_TAG        60324320cb80caec5428b0cff0c0045ed27668e8    
+	GIT_TAG        4ac9a877b70fbb821de2edbb7dd3fcad1b0a7f08
 )
 FetchContent_MakeAvailable(roar)
